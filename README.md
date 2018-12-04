a661f291cdc2afc33ceb696c35b43038f124f4d9
Mike <chenzhengzhe@maowan.com>
2018-12-04 09:58:58 +0800

    Init.

---
+++ README.md
@@ -0,0 +1,102 @@
+# laravel-api-response
+
+Laravel API 响应封装
+
+## 框架要求
+
+Laravel >= 5.5
+
+## 安装
+
+```shell
+composer require "gzoran/laravel-api-response:~1.0"
+```
+
+## 使用
+
+在你的控制器上使用 ApiResponseTrait，即可在控制器使用封装的响应方法。
+
+```php
+class ApiController extends Controller
+{
+    use ApiResponseTrait;
+    
+    public function foo()
+    {
+        return $this->ok();
+    }
+    
+    ···
+}
+```
+
+## 响应方法
+
+> 当 data 参数为空时，默认返回以 message 为键，方法名为内容的 json 响应。如果你想自定义键名，请重载 protected static $message 静态属性。
+
+```php
+
+// 基本响应
+response($data = null, int $status = 200, array $headers = [])
+
+// 成功响应 状态码 200
+ok($data = [], array $headers = [])
+
+// 已创建响应 状态码 201
+created($data = [], array $headers = [])
+
+// 请求已接受响应 状态码 202
+accepted(array $headers = [])
+
+// 无内容响应 状态码 204
+notContent(array $headers = [])
+
+// 错误请求响应 状态码 400
+badRequest($data = [], array $headers = [])
+
+// 请求未认证响应 状态码 401
+unauthorized($data = [], array $headers = [])
+
+// 请求被禁止响应 状态码 403
+forbidden($data = [], array $headers = [])
+
+// 资源未找到响应 状态码 404
+notFound($data = [], array $headers = [])
+
+// 方法不允许响应 状态码 405
+methodNotAllowed($data = [], array $headers = [])
+
+// 无法接受请求响应 状态码 406
+notAcceptable($data = [], array $headers = [])
+
+// 请求超时响应 状态码 408
+requestTimeout($data = [], array $headers = [])
+
+// 冲突响应 状态码 409
+gonfilct($data = [], array $headers = [])
+
+// 资源已不存在响应 状态码 410
+gone($data = [], array $headers = [])
+
+// 请求体过大响应 状态码 413
+payloadTooLarge($data = [], array $headers = [])
+
+// 请求 URI 过长响应 状态码 414
+requestURITooLong($data = [], array $headers = [])
+
+// 媒体类型不被支持响应 状态码 415
+unsupportedMediaType($data = [], array $headers = [])
+
+// 语义错误响应 状态码 422
+unprocessableEntity($data = [], array $headers = [])
+
+// 请求次数过多响应 状态码 429
+tooManyRequest($data = [], array $headers = [])
+
+// 服务器错误响应 状态码 500
+internalServerError($data = [], array $headers = [])
+
+// 服务不可用响应 状态码 503
+serviceUnavailable($data = [], array $headers = [])
+
+```
---
+++ src/ApiResponseTrait.php
@@ -0,0 +1,350 @@
+<?php
+
+namespace Gzoran\Http;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+/**
+ * Api response trait
+ *
+ * @author Mike <zhengzhe94@gmail.com>
+ */
+trait ApiResponseTrait
+{
+    protected static $message = 'message';
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param null $data
+     * @param int $status
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function response($data = null, int $status = 200, array $headers = [])
+    {
+        return response()->json($data, $status, $headers, JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return array|\Illuminate\Http\Response
+     */
+    protected function ok($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'OK'
+            ];
+        }
+
+        if ($data instanceof ResourceCollection) {
+            return $data;
+        }
+
+        return $this->response($data, 200, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function created($data = [], array $headers = [])
+    {
+        return $this->response($data, 201, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function accepted(array $headers = [])
+    {
+        return $this->response(null, 202, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function notContent(array $headers = [])
+    {
+        return $this->response(null, 204, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function badRequest($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Bad Request'
+            ];
+        }
+
+        return $this->response($data, 400, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthorized($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Unauthorized'
+            ];
+        }
+
+        return $this->response($data, 401, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function forbidden($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Forbidden'
+            ];
+        }
+
+        return $this->response($data, 403, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function notFound($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Not Found'
+            ];
+        }
+
+        return $this->response($data, 404, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function methodNotAllowed($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Method Not Allowed'
+            ];
+        }
+
+        return $this->response($data, 405, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function notAcceptable($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Not Acceptable'
+            ];
+        }
+
+        return $this->response($data, 406, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function requestTimeout($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Request Timeout'
+            ];
+        }
+
+        return $this->response($data, 408, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function gonfilct($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Gonfilct'
+            ];
+        }
+
+        return $this->response($data, 409, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function gone($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Gone'
+            ];
+        }
+
+        return $this->response($data, 410, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function payloadTooLarge($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Payload Too Large'
+            ];
+        }
+
+        return $this->response($data, 413, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function requestURITooLong($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Request-URI Too Long'
+            ];
+        }
+
+        return $this->response($data, 414, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function unsupportedMediaType($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Unsupported Media Type'
+            ];
+        }
+
+        return $this->response($data, 415, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function unprocessableEntity($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Unprocessable Entity'
+            ];
+        }
+
+        return $this->response($data, 422, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function tooManyRequest($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Too Many Request'
+            ];
+        }
+
+        return $this->response($data, 429, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function internalServerError($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Internal Server Error'
+            ];
+        }
+
+        return $this->response($data, 500, $headers);
+    }
+
+    /**
+     * @author Mike <zhengzhe94@gmail.com>
+     * @param array $data
+     * @param array $headers
+     * @return \Illuminate\Http\Response
+     */
+    protected function serviceUnavailable($data = [], array $headers = [])
+    {
+        if (empty($data)) {
+            $data = [
+                static::$message => 'Service Unavailable'
+            ];
+        }
+
+        return $this->response($data, 503, $headers);
+    }
+}
